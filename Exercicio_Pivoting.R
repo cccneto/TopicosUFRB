8a842fbb5f48632ac21852e72c82073f05169284
Claudiano Neto <cneto@ufrb.edu.br>
2021-07-30 09:33:57 -0300

    ajustes

---
+++ Exercicio_Pivoting.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+
+# Exercisio ---------------------------------------------------------------
+
+Usando a prosa, descreva como as variáveis e observações 
+são organizadas em cada uma das tabelas de amostra.
+
+Na tabela table1, cada linha representa uma combinação 
+(país, ano). As colunas casos e população contêm os valores dessas variáveis.
